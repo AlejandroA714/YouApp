f4ca206b0f3cd88ca3ddf087bb78e9996b9c1bb8
valejo <alejandroalejo714@gmail.com>
2021-09-10 15:19:58 -0600

    [YouApp] - Infrastructure and inheritance

---
+++ services-instances/authentication-server/src/main/java/sv/com/udb/services/authentication/AuthenticationApplication.java
@@ -0,0 +1,14 @@
+package sv.com.udb.services.authentication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication(scanBasePackages = "sv.com")
+public class AuthenticationApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(AuthenticationApplication.class)
+                .registerShutdownHook(true).run(args);
+    }
+
+}
